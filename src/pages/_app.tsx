9631efca49a5569eca96200d23d6b7d7be385666
Pedro Olavo <olavo.dev@proton.me>
2025-11-02 16:06:42 -0300

    feat: create advice page and adjust font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,23 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Quattrocento_Sans, Rozha_One } from "next/font/google";
+
+const quattrocentoSans = Quattrocento_Sans({
+  variable: "--font-quattrocento-sans",
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
+
+const rozhaOne = Rozha_One({
+  variable: "--font-rozha-one",
+  subsets: ["latin"],
+  weight: "400",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={`${rozhaOne.variable} ${quattrocentoSans.variable}`}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
